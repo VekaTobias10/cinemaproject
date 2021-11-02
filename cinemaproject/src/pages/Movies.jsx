241c90694c986bf891c8e3cad4178d575cf61310
VekaTobias10 <vekatobias@outlook.es>
2021-11-02 11:30:24 +0100

    últimos cambios

---
+++ cinemaproject/src/pages/Movies.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import "../App.css";
+import { appConfig } from "../appConfig";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+export const Movies = () => {
+
+    const [movies, setMovies] = useState([]);
+    const { BEARER_TOKEN, BASE_URL } = appConfig;
+  
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${BEARER_TOKEN}`,
+      },
+    };
+  
+    useEffect(() => {
+        fetch(`${BASE_URL}/movie`, options)
+        .then((r) => r.json())
+        .then((m) => setMovies(m.docs))
+        .catch(() => {
+          toast.error("Error del fetch")
+          console.log("errores")
+        });
+    })
+   
+    return(
+        <div className="main_container">
+      <h1 className='title_header'>Cinema Project</h1>
+      <div className="card_box_container">
+        {movies.map((c) => (
+          <div className="card_movie">
+            <p className="name">
+              <span className="subtitle">Name:</span> {c.name}
+            </p>
+            <p className="duration">
+              <span className="subtitle">Duration:</span> {c.runtimeInMinutes} min
+            </p>
+            <p className="score">
+              <span className="subtitle">Score: </span> {c.rottenTomatoesScore}
+            </p>
+          </div>
+        ))}
+      </div>
+      <ToastContainer />
+    </div>
+    );
+};
+
