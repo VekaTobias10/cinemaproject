c7661b0782b4bf353dcf6872896d53ee4c6116cc
VekaTobias10 <vekatobias@outlook.es>
2021-11-30 12:06:18 +0100

    pagination style and fixing bug

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,5 +1,6 @@
 export const appConfig = {
-  // BEARER_TOKEN: 'CfXrfk889Q7CB6IAOuH6',
-  BEARER_TOKEN: 'HoCqpOz66ehWXRcYbLlE',
-  BASE_URL: 'https://the-one-api.dev/v2',
+  BEARER_TOKEN: 'CfXrfk889Q7CB6IAOuH6',
+  BASE_URL: 'http://192.168.2.80:3001/v2'
+  // BEARER_TOKEN: 'HoCqpOz66ehWXRcYbLlE',
+  // BASE_URL: 'https://the-one-api.dev/v2',
 };
